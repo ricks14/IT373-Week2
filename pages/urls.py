456ed66e7240105b50f31c105d1d6afac49a401a
ricks14 <ricky14rulida@gmail.com>
2025-10-04 11:05:43 +0800

    Added Announcements html files and basic CSS. Updated the urls and views.

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,6 @@ from pages import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('announcement_list/', views.announcement_list, name='announcement_list'),
+    path('announcement/<int:id>/', views.announcement, name='announcement'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+TBA = [
+		{"id": 1, "title": "First Announcement", "content": "This is the full content of the first announcement."},
+		{"id": 2, "title": "Second Announcement", "content": "This is the full content of the second announcement."},
+		{"id": 3, "title": "Third Announcement", "content": "This is the full content of the third announcement."},
+]
+
 def home(request):
 	context = {
-		'title': 'home',
+		'title': 'Home',
 		'features': [
 			'Django',
 			'Templates',
@@ -14,3 +20,10 @@ def home(request):
 
 def about(request):
 	return render(request, 'about.html', {'title': 'About'})
+
+def announcement_list(request):
+	return render(request, 'announcement_list.html', {"announcements": TBA})
+
+def announcement(request, id):
+    announcement = next((a for a in TBA if a["id"] == id), None)
+    return render(request, 'announcement.html', {"announcement": announcement})
